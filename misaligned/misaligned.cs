ef40803ee784531c0b2298762391e6d4895cc97c
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-11-02 14:17:00 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -7,10 +7,11 @@ namespace MisalignedSpace {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int i = 0, j = 0;
+            string actualPairColorMap = string.Empty();
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
                     Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
-                      += ($"{i * 5 + j} | {majorColors[i]} | {minorColors[i]}\r\n");
+                    actualPairColorMap  += ($"{i * 5 + j} | {majorColors[i]} | {minorColors[i]}\r\n");
                 }
             }
             return i * j;
